105b50dc90399516cbc5dd1882ea136c6558d6e7
Akshay Srivatsan <srivatsan.akshay@gmail.com>
2019-07-21 23:33:10 -0700

    update website for 2019

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-$('.scrollspy').scrollSpy({
-    getActiveElement: function(name) {
-        return $('a[href="#' + name + '"]').parent().get();
-    },
-    scrollOffset: 50
-});
-$('.pin').pushpin();
-$('.modal').modal();
-$('.collapsible').collapsible();
